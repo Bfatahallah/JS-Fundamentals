0193e68018592c9af82299276fedb348dc6479eb
badr fatahallah <b.fatahallah@hotmail.fr>
2025-09-12 22:42:31 +0100

    remove argument checks and simplify output logic in 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,12 +1,4 @@
 // Get the arguments passed to the script
 const args = process.argv.slice(2);
-
-// Check if no arguments are provided
-if (args [0] === undefined) {
-    console.log("No argument");
-}
-// Check if arguments are provided, then print the first and second argument concatenated with "is" in between
-else {
+// print the first argument, followed by "is", followed by the second argument
     console.log(args[0] + " is " + args[1]);
-}
-
