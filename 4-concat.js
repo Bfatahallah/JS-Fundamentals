874580d65cc1e82e7ca71cd8a585a0fb19e626f4
badr fatahallah <b.fatahallah@hotmail.fr>
2025-09-12 22:36:13 +0100

    modifications to 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2);
 if (args [0] === undefined) {
     console.log("No argument");
 }
-// Check if arguments are provided, then print the first argument
+// Check if arguments are provided, then print the first and second argument concatenated with "is" in between
 else {
     console.log(args[0] + " is " + args[1]);
 }
