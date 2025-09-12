16b3998c997449210dae3e825a539d569b9ff266
Bfatahallah <b.fatahallah@hotmail.fr>
2025-09-12 21:55:58 +0100

    added 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,18 @@
+// Get the arguments passed to the script
+const args = process.argv.slice(2);
+
+// Check if no arguments are provided
+if (args.length === 0) {
+    console.log("No argument");
+}
+// Check if only one argument is provided
+else if (args.length === 1) {
+    console.log("Argument found");
+    console.log(args[0]);
+}
+// 'Otherwise' Check if two or more arguments are provided
+else  {
+    console.log("Arguments found");
+    console.log(args[0]);
+    console.log(args[1]);
+}
