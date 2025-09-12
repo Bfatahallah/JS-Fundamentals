ee07a45afa61a6d72e2bc5f9c29436fafb0cdca7
badr fatahallah <b.fatahallah@hotmail.fr>
2025-09-12 22:34:43 +0100

    add argument handling to print (arg0) is (arg1)

---
+++ 4-concat.js
@@ -0,0 +1,12 @@
+// Get the arguments passed to the script
+const args = process.argv.slice(2);
+
+// Check if no arguments are provided
+if (args [0] === undefined) {
+    console.log("No argument");
+}
+// Check if arguments are provided, then print the first argument
+else {
+    console.log(args[0] + " is " + args[1]);
+}
+
