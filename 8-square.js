8c0ea6aefe0dfc6e5cd97500fdc574c6a508eefe
badr fatahallah <b.fatahallah@hotmail.fr>
2025-09-13 20:46:08 +0100

    made some adjustment to print X in Cap

--- 8-square.js
+++ 8-square.js
@@ -9,6 +9,6 @@ if (isNaN(num)) {
     console.log("Missing size");
 } else if (num > 0) {
     for (let i = 0; i < num; i++) {
-    console.log("x".repeat(num));
+    console.log("X".repeat(num));
     }
 }
