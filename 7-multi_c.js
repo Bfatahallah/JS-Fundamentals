27cba1f5c9eb5608a26b899c95d5f41a8f8c69ea
badr fatahallah <b.fatahallah@hotmail.fr>
2025-09-13 20:16:56 +0100

    add 7-multi_c.js to print "C is fun" x [arg] times

---
+++ 7-multi_c.js
@@ -0,0 +1,13 @@
+// Get the arguments passed to the script
+const args = process.argv.slice(2);
+
+// Try to convert the first argument to an integer
+const num = parseInt(args[0], 10);
+
+// Check if the conversion was successful
+if (isNaN(num)) {
+    console.log("Missing number of occurrences");
+} else {
+    for (let i = 0; i < num; i++) 
+    console.log("C is fun " + num);
+}
