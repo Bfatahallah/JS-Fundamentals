4d5dbc152aaa5000ac3ef64f8d6192fc11978e8e
badr fatahallah <b.fatahallah@hotmail.fr>
2025-09-13 20:21:31 +0100

    maded changes to correct ouptut when argument is a negative number

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -7,7 +7,7 @@ const num = parseInt(args[0], 10);
 // Check if the conversion was successful
 if (isNaN(num)) {
     console.log("Missing number of occurrences");
-} else {
+} else if (num > 0) {
     for (let i = 0; i < num; i++) 
     console.log("C is fun " + num);
 }
